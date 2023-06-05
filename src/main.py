c040be7880d2cb53661b844f677ebc5766658462
CullenStClair <cullen.st.clair2@gmail.com>
2023-06-05 17:10:04 -0400

    regrouped output handling
    Signed-off-by: CullenStClair <cullen.st.clair2@gmail.com>

--- src/main.py
+++ src/main.py
@@ -10,19 +10,13 @@ def main():
 
     args = parse_args()
 
-    # Handle guaranteed file path arguments
+    # Handle input file path
     in_file = Path(args.in_file)
-    if not in_file.is_file():  # check if input file exists
+    if not in_file.is_file():  # Check if input file exists
         print(f"File not found: {in_file}")
         exit(1)
-    out_file = in_file.parent.joinpath(args.out_file)  # output to same directory as input file
-    i = 1  # if output file already exists, append number to filename
-    stem = out_file.stem
-    while out_file.exists():
-        out_file = in_file.parent.joinpath(f"{stem}-{i}{out_file.suffix}")
-        i += 1
 
-    # read the input image contents into a numpy array
+    # Read the input image contents into a numpy array
     try:
         with Image.open(in_file) as img:
             img_arr = np.array(img)
@@ -33,10 +27,23 @@ def main():
         print(f"File is not a valid image: {in_file}")
         exit(1)
 
-    # check if the image has an alpha channel
+    # Check if the image has an alpha channel
     has_alpha = True if img_arr.shape[2] == 4 else False
 
-    # save the output image
+    # TODO Handle operations
+    #
+    #
+    #
+
+    # Handle output file path
+    out_file = in_file.parent.joinpath(args.out_file)  # Output to same directory as input file
+    i = 1  # If output file already exists, append number to filename
+    stem = out_file.stem
+    while out_file.exists():
+        out_file = in_file.parent.joinpath(f"{stem}-{i}{out_file.suffix}")
+        i += 1
+
+    # Save the output image
     try:
         with Image.fromarray(img_arr) as img:
             img.save(out_file)
@@ -46,7 +53,7 @@ def main():
     except OSError:
         print(f"Unable to save file: {out_file}")
         if has_alpha:
-            print("Input image has transparency: Try saving as .png or .webp")
+            print("Image has transparency. Try saving as another format")
         exit(1)
 
 
