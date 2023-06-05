40df3c6179ee7d7c0ac2dba747ae369af1e8a242
CullenStClair <cullen.st.clair2@gmail.com>
2023-06-05 00:44:37 -0400

    Fleshed out argument parsing
    Signed-off-by: CullenStClair <cullen.st.clair2@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,61 +1,12 @@
-from argparse import ArgumentParser
-
 import numpy as np
 import PIL.Image as Image
+
+from parse_args import parse_args
 
 
 def main():
 
     args = parse_args()
-
-
-def parse_args():
-    """Parse command-line arguments, handling multiple sub-commands with options:
-
-    grayscale-----
-    threshold-----
-    sepia
-    blur
-    sharpen
-    edge
-    merge-----
-    mirrorV-----
-    mirrorH-----
-    rotateCW-----
-    rotateCCW-----
-    crop-----
-    resize
-    matrix
-    chain
-    """
-
-    parser = ArgumentParser(description="Simple image processing")
-
-    parser.add_argument('input', metavar="<input-file>", help='Input image file path')
-    parser.add_argument('output', metavar="<output-file>", help='Output image file name')
-
-    # Setting dest='operation' means that the Namespace returned from
-    # parse_args() will contain an attribute called 'operation' which
-    # indicates which sub-command was specified
-    subparsers = parser.add_subparsers(help='Operation', dest='operation', required=True)
-
-    parser_op_grayscale = subparsers.add_parser('grayscale', help='Grayscale the image')
-    parser_op_threshold = subparsers.add_parser('threshold', help='Threshold filter the image')
-    parser_op_sepia = subparsers.add_parser('sepia', help='Sepia filter the image')
-    parser_op_blur = subparsers.add_parser('blur', help='Box blur the image')
-    parser_op_sharpen = subparsers.add_parser('sharpen', help='Sharpen the image')
-    parser_op_edge = subparsers.add_parser('edge', help='Apply edge detection to the image')
-    parser_op_merge = subparsers.add_parser('merge', help='Merge two images')
-    parser_op_mirrorV = subparsers.add_parser('mirrorV', help='Mirror the image across the vertical axis')
-    parser_op_mirrorH = subparsers.add_parser('mirrorH', help='Mirror the image across the horizontal axis')
-    parser_op_rotateCW = subparsers.add_parser('rotateCW', help='Rotate the image clockwise')
-    parser_op_rotateCCW = subparsers.add_parser('rotateCCW', help='Rotate the image counter-clockwise')
-    parser_op_crop = subparsers.add_parser('crop', help='Crop the image')
-    parser_op_resize = subparsers.add_parser('resize', help='Resize the image')
-    parser_op_matrix = subparsers.add_parser('matrix', help='Apply a matrix to the image')
-    parser_op_chain = subparsers.add_parser('chain', help='Chain multiple operations together')
-
-    return parser.parse_args()
 
 
 if __name__ == "__main__":
---
+++ src/parse_args.py
@@ -0,0 +1,175 @@
+from argparse import ArgumentParser, ArgumentTypeError
+
+
+def parse_args():
+    """Parse command-line arguments, handling multiple sub-commands with options:
+
+    boxblur       [-r, --radius <blur-radius>] [-i, --iterations <iterations>]
+    chain         <operation> [<operation> ...]
+    composite         <input-file-2> [-a, --alpha <alpha-value>] [-o, --offset <x-offset> <y-offset>]
+    concat        [input-file-2] {--above | --below | --left | --right} {--scale | --crop | --fill}
+    crop          <x1> <y1> <x2> <y2>
+    edge          [-t, --thin]
+    grayscale     
+    invert
+    kernel        <kernel-file> [-i, --iterations <iterations>]
+    mirrorV
+    mirrorH
+    resize        <width> <height>
+    rotateCW      [-t, --turns <turns>]
+    rotateCCW     [-t, --turns <turns>]
+    sepia         
+    sharpen       [-s, --strength <sharpness-strength>]
+    threshold     [-t, --threshold <threshold-value>] [-i, --invert]
+    """
+
+    parser = ArgumentParser(description="Simple image processing")
+
+    # Common required arguments
+    parser.add_argument("in_file", metavar="<image-path>", help="Input image file path")
+    parser.add_argument("out_file", metavar="<output-file>",
+                        help="Output image name with file extension (saved in same directory as input file)")
+
+    # Setting dest="operation" means that the Namespace returned from
+    # parse_args() will contain an attribute called "operation" which
+    # indicates which sub-command was specified
+    subparsers = parser.add_subparsers(dest="operation", required=True, help="Operation")
+
+    # Sub-commands
+    # Boxblur
+    parser_op_boxblur = subparsers.add_parser("boxblur", help="Blur the image with a box blur")
+    parser_op_boxblur.add_argument("-r", "--radius", metavar="<blur-radius>", type=positive_int,
+                                   default=1, help="Radius of pixel sampling (default: 1)")
+    parser_op_boxblur.add_argument("-i", "--iterations", metavar="<blur-iterations>",
+                                   type=positive_int, default=1, help="Number of blur passes (default: 1)")
+
+    # Chain (only operations that do not require additional arguments can be chained)
+    # Valid operations: grayscale, threshold, sepia, blur, sharpen, edge, invert, mirrorV, mirrorH, rotateCW, rotateCCW, concat
+    parser_op_chain = subparsers.add_parser("chain", help="Apply multiple operations (without arguments) to the image")
+    parser_op_chain.add_argument("operations", metavar="<operation>", nargs="+", help="Operations to apply in sequence",
+                                 choices=["grayscale", "threshold", "sepia", "blur", "sharpen", "edge",
+                                          "invert", "mirrorV", "mirrorH", "rotateCW", "rotateCCW", "concat"])
+
+    # Composite
+    parser_op_merge = subparsers.add_parser("composite", help="Composite an image over another")
+    parser_op_merge.add_argument("in_file2", metavar="<top-image-path>",
+                                 help="File path of image to be placed over first image")
+    parser_op_merge.add_argument("-a", "--alpha", metavar="<alpha-value>", type=valid_alpha,
+                                 default=0.5, help="Transparency of top image [0-1] (default: 0.5)")
+    parser_op_merge.add_argument("-o", "--offset", metavar="<x-offset> <y-offset>", nargs=2, type=int,
+                                 default=[0, 0], help="Offset of top image from top-left corner of bottom image (default: 0 0)")
+
+    # Concat
+    parser_op_concat = subparsers.add_parser("concat", help="Concatenate two images or tile the same image")
+    # The second image is optional, in which case the first image is tiled
+    parser_op_concat.add_argument("in_file2", metavar="<second-image-path>", default=None,
+                                  nargs="?", help="File path of image to be placed next to first image (optional)")
+    placement_group = parser_op_concat.add_mutually_exclusive_group(required=True)
+    # The default concatenation placement is "below"
+    placement_group.add_argument("--above", action="store_const", dest="placement",
+                                 const="above", help="Place image 2 above image 1")
+    placement_group.add_argument("--below", action="store_const", dest="placement",
+                                 const="below", help="Place image 2 below image 1 (default)", default="below")
+    placement_group.add_argument("--left", action="store_const", dest="placement",
+                                 const="left", help="Place image 2 to the left of image 1")
+    placement_group.add_argument("--right", action="store_const", dest="placement",
+                                 const="right", help="Place image 2 to the right of image 1")
+    mode_group = parser_op_concat.add_mutually_exclusive_group(required=True)
+    # The default concatenation mode is "fill"
+    mode_group.add_argument("--scale", action="store_const", dest="mode", const="scale",
+                            help="Scale the second image to match the bordering dimension of the first image (default)")
+    mode_group.add_argument("--crop", action="store_const", dest="mode", const="crop",
+                            help="Crop the second image to match the bordering dimension of the first image")
+    mode_group.add_argument("--fill", action="store_const", dest="mode", const="fill", default="fill",
+                            help="Fill unused space with the average colour of the second image")
+
+    # Crop
+    parser_op_crop = subparsers.add_parser("crop", help="Crop the image to within the given coordinates")
+    parser_op_crop.add_argument("x1", metavar="<x1>", type=non_negative_int, help="X coordinate of top-left corner")
+    parser_op_crop.add_argument("y1", metavar="<y1>", type=non_negative_int, help="Y coordinate of top-left corner")
+    parser_op_crop.add_argument("x2", metavar="<x2>", type=non_negative_int, help="X coordinate of bottom-right corner")
+    parser_op_crop.add_argument("y2", metavar="<y2>", type=non_negative_int, help="Y coordinate of bottom-right corner")
+
+    # Edge
+    parser_op_edge = subparsers.add_parser("edge", help="Apply edge detection to the image")
+    parser_op_edge.add_argument("-t", "--thin", action="store_true", default=False,
+                                help="Apply edge thinning post-processing")
+
+    # Grayscale
+    parser_op_grayscale = subparsers.add_parser("grayscale", help="Grayscale the image")
+
+    # Invert
+    parser_op_invert = subparsers.add_parser("invert", help="Invert the image colours")
+
+    # Kernel
+    parser_op_kernel = subparsers.add_parser("kernel", help="Perform a convolution on the image with the given kernel")
+    parser_op_kernel.add_argument("kernel_file", metavar="<kernel-file>",
+                                  help="File path of kernel/matrix to use in convolution (comma-separated values in .txt/.csv)")
+    parser_op_kernel.add_argument("-i", "--iterations", metavar="<iterations>", type=positive_int, default=1,
+                                  help="Number of convolution passes (default: 1)")
+
+    # Mirror vertical and horizontal
+    parser_op_mirrorV = subparsers.add_parser("mirrorV", help="Mirror the image about the vertical axis")
+    parser_op_mirrorH = subparsers.add_parser("mirrorH", help="Mirror the image about the horizontal axis")
+
+    # Resize
+    parser_op_resize = subparsers.add_parser("resize", help="Resize the image to the given dimensions")
+    parser_op_resize.add_argument("width", metavar="<width>", type=positive_int, help="New image width")
+    parser_op_resize.add_argument("height", metavar="<height>", type=positive_int, help="New image height")
+
+    # Rotate clockwise and counter-clockwise
+    parser_op_rotateCW = subparsers.add_parser("rotateCW", help="Rotate the image 90 degrees clockwise")
+    parser_op_rotateCW.add_argument("-t", "--turns", metavar="<turns>", type=positive_int,
+                                    default=1, help="Number of clockwise turns (default: 1)")
+    parser_op_rotateCCW = subparsers.add_parser("rotateCCW", help="Rotate the image 90 degrees counter-clockwise")
+    parser_op_rotateCCW.add_argument("-t", "--turns", metavar="<turns>", type=positive_int,
+                                     default=1, help="Number of counter-clockwise turns (default: 1)")
+
+    # Sepia
+    parser_op_sepia = subparsers.add_parser("sepia", help="Sepia tone the image")
+
+    # Sharpen
+    parser_op_sharpen = subparsers.add_parser("sharpen", help="Sharpen the image")
+    parser_op_sharpen.add_argument("-s", "--strength", metavar="<multiplier>",
+                                   type=positive_float, default=5, help="Sharpening strength multiplier (default: 5)")
+
+    # Threshold
+    parser_op_threshold = subparsers.add_parser("threshold", help="Threshold filter the image")
+    parser_op_threshold.add_argument("-t", "--threshold", metavar="<threshold-value>", type=int,
+                                     choices=range(0, 256), default=128, help="Threshold value [0-255] (default: 128)")
+    parser_op_threshold.add_argument("-i", "--invert", action="store_true",
+                                     default=False, help="Invert the threshold operation (above threshold -> black)")
+
+    return parser.parse_args()
+
+
+def non_negative_int(value):
+    """Check that the value is a non-negative integer"""
+    ivalue = int(value)
+    if ivalue < 0:
+        raise ArgumentTypeError("%s is an invalid non-negative int value" % value)
+    return ivalue
+
+
+def positive_int(value):
+    """Check that the value is a positive integer"""
+    ivalue = int(value)
+    if ivalue <= 0:
+        raise ArgumentTypeError("%s is an invalid positive int value" % value)
+    return ivalue
+
+
+def positive_float(value):
+    """Check that the value is a positive float"""
+    fvalue = float(value)
+    if fvalue <= 0:
+        raise ArgumentTypeError("%s is an invalid positive float value" % value)
+    return fvalue
+
+
+def valid_alpha(value):
+    """Check that the value is a float in the range [0, 1]"""
+    fvalue = float(value)
+    if fvalue < 0 or fvalue > 1:
+        raise ArgumentTypeError("%s is an invalid alpha value (must be in range [0, 1])" % value)
+    return fvalue
